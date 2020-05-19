16a9b4f8d045294f28afb4931370c9d468e12824
David Strauch <dmaster18@gmail.com>
2020-05-19 23:28:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect_to '/account'
     else
-      redirect_to '/login'
+      erb :error
     end
   end
 
