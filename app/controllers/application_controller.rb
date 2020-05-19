231126f4731e6a5a97bbefcdab5d73d687dce880
David Strauch <dmaster18@gmail.com>
2020-05-19 23:26:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/login' do 
-end
+  end
    
    post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
